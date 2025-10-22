d42862c4542763972216025807ef7fcd01cd8eda
Kirubel Assefa <kirubelassefa92@gmail.com>
2025-10-22 14:59:19 -0700

    feat(auth): Implement user login logic

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,9 @@
 import type { Request, Response } from "express"
 import { prismaClient } from "../index.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config/secrets.js";
+
 export const signUp = async(req : Request, res: Response) => {
    const { name , email , password } = req.body;
    try {
@@ -36,5 +39,35 @@ export const signUp = async(req : Request, res: Response) => {
         console.error("Sign up error:", error);
         return res.status(500).json({ message : "Internal server error" })
    };
+};
 
+export const login = async(req:Request , res: Response) => {
+    try {
+        const { email, password } = req.body;
+        if(!email || !password) {
+            return res.status(400).json({message : "Email and Password are required!"});
+        };
+        const user = await prismaClient.user.findUnique({where : {email}});
+        if(!user) {
+            return res.status(400).json({message : "Invalid Email or Password"})
+        };
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid) {
+            return res.status(400).json({message : "Invalid Email or Password"})
+        };
+        const token = jwt.sign(
+            {id : user.id , email : user.email},
+            JWT_SECRET,
+            {expiresIn : "1d"}
+        );
+        const { password : _, ...safeUser } = user;
+        return res.status(200).json({
+            message : "Login successful",
+            user : safeUser,
+            token
+        });
+    } catch (error) {
+        console.error("Login error", error);
+        return res.status(500).json({ message : "Internal server error"})
+    }
 }
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { signUp } from "../controllers/auth.controller.js";
+import { login, signUp } from "../controllers/auth.controller.js";
 const authRoute: Router = Router();
 
 authRoute.post("/signup", signUp);
+authRoute.post("/login", login)
 
 export default authRoute;       
