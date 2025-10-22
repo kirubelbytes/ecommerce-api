b36a7aa0fb7149ee41a033aa4d7e5c3dfe56b896
Kirubel Assefa <kirubelassefa92@gmail.com>
2025-10-21 23:49:47 -0700

    chore: move Prisma to dependencies, add type annotations for Express Request/Response

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import type { Express, Request ,Response } from "express";
 import dotenv from "dotenv";
+
 dotenv.config();
-const app = express();
+const app:Express = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.get("/", (_, res) => res.send("API running..."));
+app.get("/", (req:Request, res:Response) => res.send("API running..."));
 
 app.listen(3000, () => console.log(`Server on http://localhost:${PORT}`));
