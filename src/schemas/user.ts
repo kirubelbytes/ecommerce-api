658fceae5abdc7423f8b6e2b3dd47ac3d9beb00c
Kirubel Assefa <kirubelassefa92@gmail.com>
2025-10-27 12:17:10 -0700

    refactor(auth): Improve the signup and login schema with stricter validation rule

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,12 +1,12 @@
 import z from "zod";
 
 export const singUpSchema = z.object({
-    name: z.string(),
-    email : z.string(),
-    password : z.string().min(4,  "Password must be atleast 8 character long")
+    name: z.string().min(1, "Name is required").trim(),
+    email : z.string().email("Invalid email address"),
+    password : z.string().min(8,  "Password must be atleast 8 character long")
 });
 
 export const logInSchema = z.object({
-    email : z.string(),
-    password : z.string().min(4, "Password must be atleast 8 character long")
+    email : z.string().email("Invalid email eddress"),
+    password : z.string().min(8, "Password must be atleast 8 character long")
 });
