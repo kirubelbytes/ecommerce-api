e1b36ffc456264479c48c4b6a4596165ad7bb09b
Kirubel Assefa <kirubelassefa92@gmail.com>
2025-10-30 02:08:15 -0700

    feat(auth): add /me route with functionality

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -57,3 +57,7 @@ export const login = async(req:Request , res: Response, next : NextFunction) => 
             token
         });
 }
+
+export const me = (req: Request, res: Response) => {
+    res.json(req.user)
+}
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { login, signUp } from "../controllers/auth.controller.js";
+import { login, me, signUp } from "../controllers/auth.controller.js";
 import { errorHandler } from "../errorHandler.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 const authRoute: Router = Router();
 
 authRoute.post("/signup",errorHandler(signUp));
-authRoute.post("/login", errorHandler(login))
+authRoute.post("/login", errorHandler(login));
+authRoute.get('/me', [authMiddleware], errorHandler(me))
 
 export default authRoute;       
