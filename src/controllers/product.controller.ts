2bc510b020e825114099dfbd6f83d616209f1bd3
Kirubel Assefa <kirubelassefa92@gmail.com>
2025-10-31 06:25:47 -0700

    feat(product): add updateProduct controller

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,6 +1,8 @@
 import express, { type NextFunction, type Request, type Response } from "express";
 import { prismaClient } from "../index.js";
 import { createProductSchema } from "../schemas/user.js";
+import { NotFoundException } from "../exceptions/NotFoundException.js";
+import { ErrorCode } from "../exceptions/BaseError.js";
 
 export const createProduct = async (req: Request, res: Response, next: NextFunction) => {
     const validatedData = createProductSchema.parse(req.body)
@@ -13,3 +15,38 @@ export const createProduct = async (req: Request, res: Response, next: NextFunct
     });
     res.json(product);
 };
+
+
+export const updateProduct = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const productData = req.body;
+
+    if (Array.isArray(productData.tags)) {
+      productData.tags = productData.tags.join(",");
+    }
+
+    const updatedProduct = await prismaClient.product.update({
+      where: { id: Number(req.params.id) },
+      data: productData,
+    });
+
+    res.json(updatedProduct);
+  } catch (err: any) {
+    if (err.code === "P2025") {
+      return next(new NotFoundException("Product not found", ErrorCode.PRODUCT_NOT_FOUND));
+    }
+    next(err);
+  }
+};
+
+export const deleteProduct = async(req: Request, res: Response) => {
+
+}
+
+export const listProducts = async(req: Request, res: Response) => {
+
+}
+
+export const getProductById = async(req: Request, res: Response) => {
+
+}
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express"
 import { errorHandler } from "../errorHandler.js";
-import { createProduct } from "../controllers/product.controller.js";
+import { createProduct, updateProduct } from "../controllers/product.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import adminMiddleware from "../middlewares/admin.middleware.js";
 
 const productRoutes: Router = Router();
 
 productRoutes.post('/',[authMiddleware, adminMiddleware], errorHandler(createProduct))
-
+productRoutes.put('/:id',[authMiddleware, adminMiddleware], errorHandler(updateProduct))
 export default productRoutes;
